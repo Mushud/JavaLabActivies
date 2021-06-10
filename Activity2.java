84795de2494d11bc0384d9988a3e968338e9dff8
Mushud Kofi <mushudkofiabuu@gmail.com>
2021-06-10 21:57:18 +0000

    Update Activity2.java

--- Activity2.java
+++ Activity2.java
@@ -5,11 +5,6 @@ import java.util.Scanner;
  */
 public class Activity2 {
     
-    private static  final int  CLOTHING_CODE = 1;
-    private static final int  OTHER_TYPE =2;
-
-    private static final String CLOTHING_PRICER = "Regina";
-    private static final String OTHER_PRICER = "Marco"; 
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
@@ -18,18 +13,16 @@ public class Activity2 {
         System.out.println("Choose 1 for Clothing Donation and 2 for any other donation type: ");
 
         int donationType = scanner.nextInt();
-        checkDonationType(donationType);
+        if(donationType == 1){
+            System.out.println("Reginina handles such donations");
+            System.out.println("Thank You");
+        }else if (donationType == 2){
+            System.out.println("Marko handles such donations");
+        } else 
+            System.out.println("Invalid Donation Type \n Thank You");
         
         scanner.close();
     }
 
-    public static void checkDonationType(int donationType) {
-        if(donationType == CLOTHING_CODE){
-            System.out.println(CLOTHING_PRICER + " handles such donations");
-            System.out.println("Thank You");
-        }else if (donationType == OTHER_TYPE){
-            System.out.println(OTHER_PRICER + " handles such donations");
-        } else 
-            System.out.println("Invalid Donation Type \n Thank You");
-    }
+   
 }
